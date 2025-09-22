370ccd59484848a48368b024be33ccc3562b3b7f
Bryce? <bryce.jones2026@gbwl.org>
2025-09-22 16:40:48 +0000

    started it

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("hello world")
+# Data Collection Drills
 
 
-if __name__ == "__main__":
-    main()
+#Part A: Lists
+my_list = ["M", "A", "C", "H", "I", "N", "E"]
+print(my_list) #["M", "A", "C", "H", "I", "N", "E"]
+my_list.append("!") # added a !
+print(my_list)
+print(len(my_list))#8
+print(my_list[4:6]) # It returned the letters 4 through 6
+print(my_list[4:]) 
+print(my_list[-2:])
+my_list.remove("!")
+print(my_list)
+my_list.insert(9, "!")
+print(my_list)
+del my_list[0]
+print(my_list)
+last_item = my_list.pop()
+print(last_item)
+print(my_list)
+print("!" in my_list)
+my_list.sort(reverse=True)
+print(my_list)
+print(sorted(my_list))
+print(my_list)
+# Part B: Tuples
+my_tuple = ("red", "green", "bleen")
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[-1])
+# my_tuple[1] = "yellow"
+
+
+# What do you notice about the syntax?
+
+
+# Part C: Sets
+
+
+# Part D: Dictionaries
