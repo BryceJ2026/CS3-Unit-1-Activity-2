b7763452c350d4949721e5c1c08c8a11b5fdd896
Bryce? <bryce.jones2026@gbwl.org>
2025-09-26 13:46:43 +0000

    did something

--- main.py
+++ main.py
@@ -4,19 +4,19 @@
 #Part A: Lists
 my_list = ["M", "A", "C", "H", "I", "N", "E"]
 print(my_list) #["M", "A", "C", "H", "I", "N", "E"]
-my_list.append("!") # added a !
+my_list.append("!") # added a ! There was not a ! before, then it was added to the end
 print(my_list)
 print(len(my_list))#8
-print(my_list[4:6]) # It returned the letters 4 through 6
-print(my_list[4:]) 
-print(my_list[-2:])
-my_list.remove("!")
-print(my_list)
-my_list.insert(9, "!")
+print(my_list[4:6]) # It returned the letters 4 through 6. It works because  those numbers exist in the list, if they didn't, it wouldn't work
+print(my_list[4:]) #It returned letter 4 till it stopped. No it is not the same as above.
+print(my_list[-2:]) # It returned the last two letters It is useful because you can check if the list works
+my_list.remove("!") 
+print(my_list) # The ! was removed. It finds where it is and takes it out
+my_list.insert(2, "l") #It went exactly where instructed to. l is at index two
 print(my_list)
 del my_list[0]
-print(my_list)
-last_item = my_list.pop()
+print(my_list) #The letter in index 1
+last_item = my_list.pop() #
 print(last_item)
 print(my_list)
 print("!" in my_list)
@@ -29,6 +29,8 @@ my_tuple = ("red", "green", "bleen")
 print(my_tuple)
 print(my_tuple[0])
 print(my_tuple[-1])
+
+
 # my_tuple[1] = "yellow"
 
 
